7a4e8e1c5cba7332750bc126078bf6df9337618e
wyatt otto <wyattotto87@gmail.com>
2023-02-28 15:59:12 -0500

    fixed some readme errors

--- README.md
+++ README.md
@@ -9,7 +9,7 @@ For this Project Ive Created a random password Generator.
 
 
 ## Usage
-- Can be used to make secure randomized passwords. 
+- Will be used to make secure randomized passwords. 
 
 
 ## Resource Links
@@ -30,30 +30,4 @@ Below is links I used to assist me with the Project
  ## Deployed Link
  - https://wyattotto.github.io/Random-Password/
 
-
- # Random-Password
-
-
-## Description
-For this Project Ive Created a random password Generator. 
-- I used gained knowledge from the Java script lessons to complete this project.
-- Event listener was used to make this project 
-
-
-
-
-
-## Resource Links
-Below is links I used to assist me with the Project
-
- -https://www.w3schools.com/js/js_popup.asp
- -https://stackoverflow.com/questions/62627469/random-password-generator-with-prompts
- -https://developer.mozilla.org/en-US/docs/Web/API/Event/stopPropagation
- 
- 
- 
- 
- 
- ## Deployed Link
- - https://wyattotto.github.io/Random-Password/
 
--- assets/script.js
+++ assets/script.js
@@ -25,41 +25,31 @@ function RandomPassword() {
     var lowerCase = prompt("Do you want your password to contain lowercase letters?  yes / no ");
     var upperCase = prompt("Do you want your password to contain uppercase letters?  yes / no ");
     var num = prompt("Do you want your password to contain numbers?  yes / no");
-    
-    //Statement for the prompt for password Length
-    if (passwordLength < 8 || passwordLength > 128) {
-        alert("Password is not between 8 characters and 128 characters. ");
-        var passwordLength = prompt("Password must be between 8 and 128 characters.");
-    }
-
-    // if statements for the selection of what variables to be included in password
-    
 
     if (num ===  "yes") {
-      optionsVariable += num1;
-  } else if (num === "no"  ){
-    optionsVariable = optionsVariable
-  }
+        optionsVariable += num1;
+            } else if (num === "no"  ){
+                optionsVariable = optionsVariable}
 
     if (upperCase === "yes") {
         optionsVariable += upperCase1;
-    } else if (upperCase === "no"){
-        optionsVariable = optionsVariable
-    }
+            } else if (upperCase === "no"){
+                optionsVariable = optionsVariable}
 
     if (lowerCase === "yes") {
-      optionsVariable += lowerCase1;
-  }else if (lowerCase === "no"){
-    optionsVariable = optionsVariable
-  }
+        optionsVariable += lowerCase1;
+            }else if (lowerCase === "no"){
+                optionsVariable = optionsVariable}
 
     if (specialChar ==='yes') {
-        optionsVariable += specialChar1
+        optionsVariable += specialChar1}
+            else if (specialChar === "no")
+                {optionsVariable = optionsVariable}
 
-    }else if (specialChar === "no")
-    {
-        optionsVariable = optionsVariable
-    }
+    if (passwordLength < 8 || passwordLength > 128) {
+        alert("Password is not between 8 characters and 128 characters. ");
+            var passwordLength = prompt("Password must be between 8 and 128 characters.");
+                }
     
     else if (lowerCase === 'no' && upperCase === "no" && num === "no" && specialChar === "no") {
         alert("You must chose at least one password criteria.");
@@ -78,7 +68,7 @@ function RandomPassword() {
 
     }
 
-generateBtn.addEventListener("click", RandomPassword , )
+generateBtn.addEventListener("click", RandomPassword ,)
 
 
 
